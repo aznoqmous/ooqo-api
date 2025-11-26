d21a4c998c0106b12658bc2674cc358b5ca453a3
aznoqmous <paul.landreau931v@gmail.com>
2025-11-26 22:33:42 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import express from "express"
+import cors from "cors"
+import {PrismaClient} from "@prisma/client"
+
+const app = express()
+const port = 3000
+const prisma = new PrismaClient()
+
+app.use(cors());
+app.use(express.json());
+
+const toJson = (object)=> JSON.stringify(object, (key, value) =>
+    typeof value === 'bigint'
+        ? value.toString()
+        : value // return everything else unchanged
+    )
+
+
+app.get("/hello", (req,res)=>{
+    res.send({foo:"bar"})
+})
+
+app.post("/run", async(req,res)=>{
+    const data = req.body
+    console.log(data)
+    delete data.id
+
+    const lastResult = await prisma.Run.findFirst({
+        orderBy: {
+            Score: "desc"
+        },
+        skip: 9,
+        take: 1
+    })
+    if(!lastResult){
+        await prisma.Run.create({
+            data
+        })
+    }
+    else if(lastResult.Score < data.Score){
+        await prisma.Run.delete({
+            where: {
+                id: lastResult.id
+            }
+        })
+        await prisma.Run.create({
+            data
+        })
+    }
+    res.send(true)
+})
+
+app.get("/leaderboard", async(req,res)=>{
+    const Runs = await prisma.Run.findMany({
+        orderBy: {
+            Score: "desc"
+        }
+    })
+    res.send({Runs})
+})
+
+app.listen(port, ()=>{
+    console.log(`Listening on ${port}`)
+})
